832712281ab56527adf5b8078a58c2fa0537e8f5
Michał Malik <michalmalik950426@gmail.com>
2023-03-22 23:48:22 +0100

    AuthService created

---
+++ FrogDate.SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl='https:/localhost:5000/api/auth'
+
+constructor(private http:HttpClient) { }
+login(model:any){
+  this.http.post(this.baseUrl + 'login', model).pipe(map((response:any)=>{
+    const user = response;
+    if(user) localStorage.setItem('token',user.token)
+  } ))
+}
+
+}
