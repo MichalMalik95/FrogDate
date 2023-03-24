20c21781173ccdec5fe82d3d9fb877bed922abc3
Michał Malik <michalmalik950426@gmail.com>
2023-03-24 16:08:23 +0100

    Top bar LOGIN/LOGOUT implemented

--- FrogDate.SPA/src/app/_services/auth.service.ts
+++ FrogDate.SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,8 @@ export class AuthService {
   baseUrl='https:/localhost:5000/api/auth'
 
 constructor(private http:HttpClient) { }
-login(model:any){
-  this.http.post(this.baseUrl + 'login', model).pipe(map((response:any)=>{
+login(model:any): Observable<any> {
+  return this.http.post(this.baseUrl + '/login', model).pipe(map((response:any)=>{
     const user = response;
     if(user) localStorage.setItem('token',user.token)
   } ))
--- FrogDate.SPA/src/app/nav/nav.component.ts
+++ FrogDate.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  model:any ={};
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
-  login(){console.log(this.model)};
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log("Pomyślnie zalogowałeś się na konto");
+    },
+      error => { console.log("Błąd logowania"); }
+    )
+  };
+  loggedIn(){
+    const token=localStorage.getItem('token');
+    return !!token;
+  };
+  loggedOut(){
+    localStorage.removeItem('token');
+    console.log("Wylogowano~!")
+  };
+
 }
