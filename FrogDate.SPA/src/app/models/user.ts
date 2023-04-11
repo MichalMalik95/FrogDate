3d144e1e59f4da17630cc060ad7fb7090eb62b8f
Michał Malik <michalmalik950426@gmail.com>
2023-04-11 19:45:27 +0200

    Model User added to SPA

---
+++ FrogDate.SPA/src/app/models/user.ts
@@ -0,0 +1,43 @@
+import { Photo } from "./photo"
+
+export interface User {
+    //basic information
+    id: number
+    username: string
+    gender: string
+    age: string
+    zodiacSign: string
+    created: string
+    lastActive: string
+    city: string
+    country: string
+    
+    //specific information
+    growth: string
+    eyeColour: string
+    skinColour: string
+    martialStatus: string
+    education: string
+    profession: string
+    children: string
+    languages: string
+    motto: string
+    description: string
+    personality: string
+    lookingFor: string
+    intrests: string
+    freeTime: string
+    sports: string
+    films: string
+    music: string
+    iLike: string
+    idisslike: string
+    makesMeLaught: string
+    iFellsBestIn: string
+    friendsDescribeMe: string
+    photos: Photo[]
+    photoUrl: string
+  }
+  
+
+  
