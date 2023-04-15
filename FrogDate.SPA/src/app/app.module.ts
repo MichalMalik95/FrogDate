f5d0f5b56eaa4c1a43685b9b17ec007959cf2820
Michał Malik <michalmalik950426@gmail.com>
2023-04-15 16:39:39 +0200

    user-list component added

--- FrogDate.SPA/src/app/app.module.ts
+++ FrogDate.SPA/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
 import { AlertifyService } from './_services/alertify.service';
 import { UserService } from './_services/user.service';
+import { UserListComponent } from './users/user-list/user-list.component';
 
 @NgModule({
   declarations: [		
     AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      UserListComponent
    ],
   imports: [
     BrowserModule,
---
+++ FrogDate.SPA/src/app/users/user-list/user-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
+import { User } from 'src/app/models/user';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users:User[] | undefined;
+
+  constructor(private userService: UserService, private alertify:AlertifyService) { }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser(){
+    this.userService.getUsers().subscribe((users:User[])=>{
+      this.users=users;
+    },error=>{
+      this.alertify.error(error);
+    });
+
+  }
+}
