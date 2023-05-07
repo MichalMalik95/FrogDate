75e17e775094b42d21ec45c7aa00b9b7e4ac343b
Michal Malik <michalmalik950426@gmail.com>
2023-05-07 19:51:35 +0200

    problem with 401 error solved

--- FrogDate.SPA/src/app/users/photos/photos.component.ts
+++ FrogDate.SPA/src/app/users/photos/photos.component.ts
@@ -32,7 +32,7 @@ export class PhotosComponent implements OnInit {
   initializeUploader(){
     this.uploader=new FileUploader({
       url:this.baseUrl+'/users/'+this.authService.decodedToken.nameid+'/photos',
-      authToken:'Bearer' + localStorage.getItem('token'),
+      authToken:'Bearer ' + localStorage.getItem('token'),
       isHTML5:true,
       allowedFileType:["image"],
       removeAfterUpload:true,
